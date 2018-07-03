da05dec9179ff09c389fc765123a9efc53d664d3
brotzky <brotzky@gmail.com>
2018-07-02 18:31:37 -0700

    Changing back the to email

--- mail/index.js
+++ mail/index.js
@@ -65,7 +65,7 @@ export function internalSendMail(options) {
 
   const message = {
     from: `Narative <system@narative.co>`,
-    to: 'dennis@narative.co',
+    to: 'contact@narative.co',
     subject,
     text: internalText,
     html: internalHtml
